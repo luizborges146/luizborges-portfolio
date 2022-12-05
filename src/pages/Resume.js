38d27ead16609fea35d116ed8c1ad9bd68d67df4
luizborges146 <113717656+luizborges146@users.noreply.github.com>
2022-12-05 09:29:24 -0800

    Create the Resume component

--- src/pages/Resume.js
+++ src/pages/Resume.js
@@ -1,21 +1,65 @@
 import React from 'react';
+import '../style/Resume.css';
+import resume from '../assets/luiz-resume.pdf';
 
 export default function Resume() {
-  return (
-    <div>
-      <h1>Resume</h1>
-      <p>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed neque
-        velit, lobortis ut magna varius, blandit rhoncus sem. Morbi lacinia nisi
-        ac dui fermentum, sed luctus urna tincidunt. Etiam ut feugiat ex. Cras
-        non risus mi. Curabitur mattis rutrum ipsum, ut aliquet urna imperdiet
-        ac. Sed nec nulla aliquam, bibendum odio eget, vestibulum tortor. Cras
-        rutrum ligula in tincidunt commodo. Morbi sit amet mollis orci, in
-        tristique ex. Donec nec ornare elit. Donec blandit est sed risus feugiat
-        porttitor. Vestibulum molestie hendrerit massa non consequat. Vestibulum
-        vitae lorem tortor. In elementum ultricies tempus. Interdum et malesuada
-        fames ac ante ipsum primis in faucibus.
-      </p>
-    </div>
-  );
+	return (
+		<div className="resume">
+			<div className="skill_set">
+				<h2>Technical Proficiencies</h2>
+				<div>
+					<ul className ="list-container">
+						<li className="skills-list">
+							<abbr title="MongoDb Express React Node">MERN</abbr>/
+							<abbr title="MongoDb Express Angular Node">MEAN</abbr> Development
+						</li>
+						<li className="skills-list">
+							Java / Java <abbr title="Enterprise Edition">EE</abbr>{' '}
+							programming
+						</li>
+						<li className="skills-list">
+							Project Planning & Development{' '}
+							<abbr title="Software Development and Operations">(DevOps)</abbr>
+						</li>
+						<li className="skills-list">Full Stack Development</li>
+						<li className="skills-list">
+							<abbr title="User Experience/ User Interface">UX/UI</abbr>
+						</li>
+						<li className="skills-list">Clean Coding</li>
+						<li className="skills-list">Agile / Scrum Development</li>
+						<li className="skills-list">Testing & Troubleshooting</li>
+						<li className="skills-list">Database Development</li>
+						<li className="skills-list">Multi-threaded Programs</li>
+						<li className="skills-list">Performance optimization</li>
+						<li className="skills-list">Team Management and Collaboration</li>
+					</ul>
+				</div>
+			</div>
+
+			<div className="technologies_container">
+				<h3>Skill Set</h3>
+				<ul>
+					<li className="technologies_li">
+						<span className="technologies">FrontEnd:</span><span>  React, HTML, CSS, Graphql,{' '}
+						<abbr title="Representational State Transfer">REST</abbr>, Fetch,
+						JQuery</span> 
+					</li>
+					<li className="technologies_li">
+						<span className="technologies">BackEnd: </span><span> Express, Node, Sequelize, Mongoose & Axios</span>
+					</li>
+					<li className="technologies_li">
+						<span className="technologies"> Databases:</span><span> SQL, MySQL, MongoDB</span>
+					</li>
+					<li className="technologies_li">
+						<span className="technologies">Tools:</span><span> GitHub, Git, VS Code, Eclipse, Insomnia, MongoDB Compass,</span>
+					</li>
+				</ul>
+			</div>
+			<div className="download_Link">
+				<a className="download_aTag" href={resume} download>
+					Download Resume
+				</a>
+			</div>
+		</div>
+	);
 }
