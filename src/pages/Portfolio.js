dd11574ed34fcbf861cbad4e631acc56a8c9c29b
luizborges146 <113717656+luizborges146@users.noreply.github.com>
2022-12-04 16:42:14 -0800

    Create the Portfolio component that will load when the user click on the nav-bar

--- src/pages/Portfolio.js
+++ src/pages/Portfolio.js
@@ -1,10 +1,21 @@
 import React from 'react';
+import '../style/Portfolio.css'
+
 
 export default function Portfolio() {
   return (
-    <div class="work" id="work">
+    <div class="work" id="work-gall">
         <div class="title">
-            <h2>My Work</h2>
+            <h2>Gallery</h2>
+        </div>
+
+        <div class="box">
+
+            <ul id="port-photos">
+                <a href="https://luizborges146.github.io/google-glass/" target="_blank" rel="noopener noreferrer"><li id="pic01"><span>Google Glass</span></li></a>
+                <a href="https://luizborges146.github.io/first-homework/" target="_blank" rel="noopener noreferrer"><li id="pic02"><span>Horiseon</span></li></a>
+                <a href="https://ajmanzione.github.io/Mini-Project-2/" target="_blank" rel="noopener noreferrer"><li id="pic03"><span> CSS Snippet Cheatsheet</span></li></a>
+            </ul>
         </div>
     </div>
   );
